f2a8a95aaff75445ac9a038d3cf928e90fbbf568
Maria Choufani <mm4654801@gmail.com>
2025-08-05 11:09:31 +0300

    added 2 eventlisteners to the menubtn and the navlinks

---
+++ index.js
@@ -0,0 +1,35 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i"); /*From inside the menuBtn element, find the first <i> tag*/
+
+
+const navData = [
+  { name: "Home", href: "#home" },
+  { name: "About", href: "#about" },
+  { name: "Products", href: "#products" },
+  { name: "Contact", href: "#contact" }
+];
+
+
+// Dynamically create nav links from JSON
+navData.forEach(item => {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  a.href = item.href;
+  a.textContent = item.name;
+  li.appendChild(a);
+  navLinks.appendChild(li);
+});
+
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
